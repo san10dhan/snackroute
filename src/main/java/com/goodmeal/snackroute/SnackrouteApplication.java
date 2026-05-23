c3bd57a5e480552e76bd72b5f44c19526a8faa30
Sanket <san10dhan@gmail.com>
2026-05-23 16:20:18 +0530

    Initial commit

---
+++ src/main/java/com/goodmeal/snackroute/SnackrouteApplication.java
@@ -0,0 +1,13 @@
+package com.goodmeal.snackroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackrouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodmeal/snackroute/SnackrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodmeal.snackroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
